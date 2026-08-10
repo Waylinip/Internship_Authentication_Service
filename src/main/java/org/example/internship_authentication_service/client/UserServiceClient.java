a8a04614fd0e236d85eaa7945f5e968dfb85738b
danikmarukovitch <marukovitch3@icloud.com>
2026-08-10 09:40:15 +0200

    Add RestClient configuration and UserServiceClient

---
+++ src/main/java/org/example/internship_authentication_service/client/UserServiceClient.java
@@ -0,0 +1,23 @@
+package org.example.internship_authentication_service.client;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.internship_authentication_service.dto.UserProfileRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserServiceClient {
+
+    private final RestClient userServiceRestClient;
+
+    public void createProfile(UserProfileRequest request) {
+        userServiceRestClient.post()
+                .uri("/api/internal/users")
+                .body(request)
+                .retrieve()
+                .toBodilessEntity();
+    }
+}
